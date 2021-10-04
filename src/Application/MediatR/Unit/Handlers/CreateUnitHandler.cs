29460e80ccaa552e5d162113158f663ecb407244
Michał B <MadTiger2409@users.noreply.github.com>
2021-10-04 13:23:38 +0200

    Working on query and commands for Unit entity

--- src/Application/MediatR/Unit/Handlers/CreateUnitHandler.cs
+++ src/Application/MediatR/Unit/Handlers/CreateUnitHandler.cs
@@ -1,5 +1,7 @@
-﻿using Application.Common.Interfaces;
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
 using Application.MediatR.Unit.Commands;
+using Application.MediatR.Unit.Queries;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,12 +15,22 @@ namespace Application.MediatR.Unit.Handlers
     public class CreateUnitHandler : IRequestHandler<CreateUnitCommand, Domain.Entities.Unit>
     {
         private readonly IApplicationDbContext _context;
+        private readonly ISender _mediator;
 
-        public CreateUnitHandler(IApplicationDbContext dbContext) => _context = dbContext;
+        public CreateUnitHandler(IApplicationDbContext dbContext, ISender mediator)
+        {
+            _context = dbContext;
+            _mediator = mediator;
+        }
 
         public async Task<Domain.Entities.Unit> Handle(CreateUnitCommand request, CancellationToken cancellationToken)
         {
-            var unit = new Domain.Entities.Unit { Name = request.Name };
+            var unit = await _mediator.Send(new GetUnitByNameQuery(request.Name));
+
+            if (unit != null)
+                throw new EntityAlreadyExistsException("Unit");
+
+            unit = new Domain.Entities.Unit { Name = request.Name };
             _context.Units.Add(unit);
 
             await _context.SaveChangesAsync(cancellationToken);
---
+++ src/Application/MediatR/Unit/Handlers/GetUnitByNameHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.Interfaces;
+using Application.MediatR.Unit.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.MediatR.Unit.Handlers
+{
+    public class GetUnitByNameHandler : IRequestHandler<GetUnitByNameQuery, Domain.Entities.Unit>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetUnitByNameHandler(IApplicationDbContext context) => _context = context;
+
+        public async Task<Domain.Entities.Unit> Handle(GetUnitByNameQuery request, CancellationToken cancellationToken)
+            => await _context.Units.SingleOrDefaultAsync(x => x.Name.ToLowerInvariant().Equals(request.Name.ToLowerInvariant()));
+    }
+}
