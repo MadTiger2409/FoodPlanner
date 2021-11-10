dfcaa772cb89151fbfb5b4df185031550825060f
Michał B <MadTiger2409@users.noreply.github.com>
2021-11-11 00:32:49 +0100

    Started working on some unit tests for Domain layer.

---
+++ test/Domain.UnitTests/Tests/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using FoodPlanner.Domain.Entities;
+using FoodPlanner.Domain.UnitTests.Common.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FoodPlanner.Domain.UnitTests.Tests
+{
+    public class ProductTests
+    {
+        [Theory]
+        [DataWithoutIngredients]
+        public void Create_Product_Without_Ingredients(int id, string name)
+        {
+            // Arrange
+            Product product;
+
+            // Act
+            product = new Product { Id = id, Name = name };
+
+            // Assert
+            product.Id.Should().BePositive().And.Be(id);
+            product.Name.Should().NotBeNullOrWhiteSpace().And.Be(name);
+            product.Ingredients.Should().NotBeNull();
+        }
+    }
+}
