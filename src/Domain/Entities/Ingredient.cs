fa757ca7c7da320013fc0318ea8d9e4bc87509e8
Michał B <MadTiger2409@users.noreply.github.com>
2021-11-17 00:32:23 +0100

    Created basic unit tests for Ingredient

--- src/Domain/Entities/Ingredient.cs
+++ src/Domain/Entities/Ingredient.cs
@@ -1,12 +1,53 @@
 ﻿using FoodPlanner.Domain.Common;
+using System;
 
 namespace FoodPlanner.Domain.Entities
 {
     public class Ingredient : BaseEntity
     {
-        public float Amount { get; set; }
-        public int ProductId { get; set; }
-        public int UnitId { get; set; }
+        private float amount;
+        private int productId;
+        private int unitId;
+
+        public float Amount
+        {
+            get => amount;
+
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("'Amount' must be positive.");
+
+                amount = value;
+            }
+        }
+
+        public int ProductId
+        {
+            get => productId;
+
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentException("'ProductId' must be positive.");
+
+                productId = value;
+            }
+        }
+
+        public int UnitId
+        {
+            get => unitId;
+
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentException("'UnitId' must be positive.");
+
+                unitId = value;
+            }
+        }
+
         public int MealId { get; set; }
         public Product Product { get; set; }
         public Unit Unit { get; set; }
---
+++ test/Domain.UnitTests/Tests/IngredientTests.cs
@@ -0,0 +1,122 @@
+﻿using FluentAssertions;
+using FoodPlanner.Domain.Entities;
+using FoodPlanner.Domain.UnitTests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FoodPlanner.Domain.UnitTests.Tests
+{
+    public class IngredientTests
+    {
+        [Fact]
+        public void Succed_With_Creating_Unit()
+        {
+            // Arrange
+            Ingredient ingredient = null;
+
+            // Act
+            ingredient = new Ingredient();
+
+            // Assert
+            ingredient.Should().NotBeNull();
+            ingredient.Id.Should().Be(default);
+            ingredient.MealId.Should().Be(default);
+            ingredient.ProductId.Should().Be(default);
+            ingredient.UnitId.Should().Be(default);
+        }
+
+        [Theory]
+        [PositiveId]
+        public void Succed_With_Setting_Positive_Id(int id)
+        {
+            // Arrange
+            Ingredient ingredient = new Ingredient();
+
+            // Act
+            ingredient.Id = id;
+
+            // Assert
+            ingredient.Should().NotBeNull();
+            ingredient.Id.Should().BePositive().And.Be(id);
+        }
+
+        [Theory]
+        [NotPositiveId]
+        public void Failed_With_Setting_Not_Positive_Id(int id)
+        {
+            // Arrange
+            Ingredient ingredient = new Ingredient();
+
+            // Act
+            Action action = () => ingredient.Id = id;
+
+            // Assert
+            ingredient.Id.Should().Be(default);
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [PositiveId]
+        public void Succed_With_Setting_Positive_ProductId(int id)
+        {
+            // Arrange
+            Ingredient ingredient = new Ingredient();
+
+            // Act
+            ingredient.ProductId = id;
+
+            // Assert
+            ingredient.Should().NotBeNull();
+            ingredient.ProductId.Should().BePositive().And.Be(id);
+        }
+
+        [Theory]
+        [NotPositiveId]
+        public void Failed_With_Setting_Not_Positive_ProductId(int id)
+        {
+            // Arrange
+            Ingredient ingredient = new Ingredient();
+
+            // Act
+            Action action = () => ingredient.ProductId = id;
+
+            // Assert
+            ingredient.ProductId.Should().Be(default);
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [PositiveId]
+        public void Succed_With_Setting_Positive_UnitId(int id)
+        {
+            // Arrange
+            Ingredient ingredient = new Ingredient();
+
+            // Act
+            ingredient.UnitId = id;
+
+            // Assert
+            ingredient.Should().NotBeNull();
+            ingredient.UnitId.Should().BePositive().And.Be(id);
+        }
+
+        [Theory]
+        [NotPositiveId]
+        public void Failed_With_Setting_Not_Positive_UnitId(int id)
+        {
+            // Arrange
+            Ingredient ingredient = new Ingredient();
+
+            // Act
+            Action action = () => ingredient.UnitId = id;
+
+            // Assert
+            ingredient.UnitId.Should().Be(default);
+            action.Should().Throw<ArgumentException>();
+        }
+    }
+}
