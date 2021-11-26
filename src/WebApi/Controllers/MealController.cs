8e8d27c6c8595dca13abd7021bf9e9abc046ff50
Michał B <MadTiger2409@users.noreply.github.com>
2021-11-27 00:42:12 +0100

    Working on MealController

--- src/WebApi/Controllers/MealController.cs
+++ src/WebApi/Controllers/MealController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodPlanner.Application.MediatR.Meal.Queries;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,20 @@ namespace FoodPlanner.WebApi.Controllers
     [Route("webapi/meals")]
     public class MealController : ApiControllerBase
     {
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetMealByIdAsync(int id)
+        {
+            var meal = await Mediator.Send(new GetMealByIdQuery(id));
+
+            return Ok(meal);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMealsAsync()
+        {
+            var meals = await Mediator.Send(new GetMealsQuery());
+
+            return Ok(meals);
+        }
     }
 }
