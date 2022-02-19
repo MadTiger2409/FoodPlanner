ba326bbcf4840d57a99e8a5d194d4e4a0fd350f4
Michał B <MadTiger2409@users.noreply.github.com>
2022-02-19 01:23:55 +0100

    Added date range with limits

--- src/Application/MediatR/PlannedMeal/Handlers/GetPlannedMealsHandler.cs
+++ src/Application/MediatR/PlannedMeal/Handlers/GetPlannedMealsHandler.cs
@@ -5,6 +5,7 @@ using FoodPlanner.Application.MediatR.PlannedMeal.Queries;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,6 +24,7 @@ namespace FoodPlanner.Application.MediatR.PlannedMeal.Handlers
 
         public async Task<List<PlannedMealDto>> Handle(GetPlannedMealsQuery request, CancellationToken cancellationToken)
             => _mapper.Map<List<PlannedMealDto>>(await _context.PlannedMeals
+                .Where(x => x.ScheduledFor.Date >= request.From.Date && x.ScheduledFor.Date <= request.To.Date)
                 .Include(x => x.Meal).ThenInclude(y => y.Ingredients)
                 .Include(x => x.Meal.Ingredients).ThenInclude(y => y.Product)
                 .Include(x => x.Meal.Ingredients).ThenInclude(y => y.Unit)
