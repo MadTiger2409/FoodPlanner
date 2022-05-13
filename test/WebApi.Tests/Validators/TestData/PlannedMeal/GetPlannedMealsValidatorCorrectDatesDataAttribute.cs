745e722a19126dd00cbb314adf47cef4bf1746d0
Michał B <MadTiger2409@users.noreply.github.com>
2022-05-13 18:42:55 +0200

    Fresh tests :D

---
+++ test/WebApi.Tests/Validators/TestData/PlannedMeal/GetPlannedMealsValidatorCorrectDatesDataAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace WebApi.Tests.Validators.TestData.PlannedMeal
+{
+    public class GetPlannedMealsValidatorCorrectDatesDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { DateTime.UtcNow.Date, DateTime.UtcNow.Date };
+            yield return new object[] { DateTime.UtcNow.Date, DateTime.UtcNow.AddDays(1).Date };
+            yield return new object[] { DateTime.UtcNow.Date, DateTime.UtcNow.AddDays(31).Date };
+            yield return new object[] { DateTime.UtcNow.AddDays(2).Date, DateTime.UtcNow.AddDays(10).Date };
+        }
+    }
+}
---
+++ test/WebApi.Tests/Validators/UnitTests/PlannedMeal/GetPlannedMealsValidatorPositiveTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.TestHelper;
+using FoodPlanner.WebApi.ActionParameters.PlannedMeal;
+using FoodPlanner.WebApi.Validators.PlannedMeal;
+using System;
+using WebApi.Tests.Validators.TestData.PlannedMeal;
+using Xunit;
+
+namespace WebApi.Tests.Validators.UnitTests.PlannedMeal
+{
+    [Trait("Unit tests", "Validators")]
+    public class GetPlannedMealsValidatorPositiveTests : ValidatorTestsBase<GetPlannedMealsValidator>
+    {
+        [Theory]
+        [GetPlannedMealsValidatorCorrectDatesData]
+        public void Should_Pass_Validation_For_StartDate(DateTime fromDate, DateTime toDate)
+        {
+            var model = new GetPlannedMeals { From = fromDate, To = toDate };
+
+            var result = validator.TestValidate(model);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
