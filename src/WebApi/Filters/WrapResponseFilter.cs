0005b1b75644e3c5ae25355c1f1cc851366a710a
Michał B <MadTiger2409@users.noreply.github.com>
2023-04-03 15:13:51 +0200

    Fixed exception filter

--- src/WebApi/Filters/WrapResponseFilter.cs
+++ src/WebApi/Filters/WrapResponseFilter.cs
@@ -1,7 +1,6 @@
 ﻿using FoodPlanner.WebApi.Filters.Common;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using System.Threading.Tasks;
 
 namespace FoodPlanner.WebApi.Filters
